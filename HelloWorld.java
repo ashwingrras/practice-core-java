5f26575d4bf37d6f04b2a8585454d079cad9ab15
ashwingrras <ashwin@grrassolutions.in>
2024-11-26 18:22:16 +0530

    new file HelloWorld added

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// 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
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Wolrd");
+		System.out.println("Test Example");
+	}
+}
